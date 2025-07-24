028c83e2f789f15e287a7da4a6a460b394f70600
yasakei <yas.studios.en@gmail.com>
2025-07-24 21:41:30 +0600

    added png file instead of large gif (as demo config)

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Define the GIF source path here
-const gifSource = '/assets/background.gif'; // Example path
+const gifSource = 'https://yas.ct.ws/cdn/uploads/686806921f87f.jpg'; //You can use any types of Image formats, Including Gif
